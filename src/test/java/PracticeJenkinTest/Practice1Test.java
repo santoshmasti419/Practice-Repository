acec9a3fc305518ec3f707dd491a61aff332f380
Dhanraj <Dhanraj@LAPTOP-79C88I1L>
2022-05-31 15:23:36 +0530

    changes2

--- src/test/java/PracticeJenkinTest/Practice1Test.java
+++ src/test/java/PracticeJenkinTest/Practice1Test.java
@@ -43,7 +43,7 @@ public class Practice1Test {
 			 driver=new ChromeDriver();
 			 break;
 	      }*/
-		 //changes
+		 //changes 2
 		driver.get("http://localhost:8888");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
